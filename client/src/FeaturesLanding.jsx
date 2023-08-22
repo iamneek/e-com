df93bdda929cdd33cced5146295350d40d603387
iamneek <neekkafle29@gmail.com>
2023-08-22 14:09:36 +0545

    second commit

---
+++ client/src/FeaturesLanding.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "FeatureLanding.scss";
+const FeaturesLanding = () => {
+  return <div className="feature_landing_main"></div>;
+};
+const FeatureBox = () => {
+  return <div></div>;
+};
+
+export default FeaturesLanding;
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import HomeSlider from "./HomeSlider";
 import "./Home.scss";
+import Adidas from "./assets/adidas.png";
+import Calvin from "./assets/calvin.png";
+import Chanel from "./assets/chanel.png";
+import DandDG from "./assets/dandg.png";
+import Gucci from "./assets/gucci.png";
+import Guess from "./assets/guess.png";
+import Levi from "./assets/levi.png";
+import Vercase from "./assets/Vercase.png";
+
 const Home = () => {
   return (
     <div className="HomeComp">
       <HomeSlider />
       <div className="Brands">
         <h1>Brands</h1>
+        <div className="brand_logos">
+          <img src={Chanel} alt="Chanel" />
+          <img src={Calvin} alt="Calvin" />
+          <img src={Guess} alt="Guess" />
+          <img src={Gucci} alt="Gucci" />
+          <img src={DandDG} alt="D & DG" />
+          <img src={Adidas} alt="Adidas" />
+          <img src={Levi} alt="Levi" />
+          <img src={Vercase} alt="Vercase" className="vercase" />
+        </div>
       </div>
     </div>
   );
